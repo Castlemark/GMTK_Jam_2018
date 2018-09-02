ced6ffeec3597efaf8fa797458c9d6fb3bcd8dc3
Marc Castells i Güell <marc.castells.guell@gmail.com>
2018-09-02 14:08:48 +0200

    Races can be finished
    It'll wait 3 second before letting players move, when one player reaches the end it will freze action and announce winning player

---
+++ GMTK_Jam_2018/Assets/Scripts/Utils/FinishTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour {
+
+    public int victorious_player = 0;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.name.Equals("player_1")) {
+            victorious_player = 1;
+        } else {
+            victorious_player = 2;
+        }
+    }
+
+    public bool SomeoneHasFinished() {
+        return victorious_player != 0;
+    }
+}
